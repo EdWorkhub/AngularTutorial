46973162bba91a7a28dcbeb21e8d18655f5022c3
Ed <ed.ferris@workhub.com>
2025-05-29 07:46:33 -0600

    may29

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from "@angular/core";
-import { DUMMY_USERS } from "../dummy-users";
+import { Component, Output, Input, EventEmitter } from "@angular/core";
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
     selector: 'app-user',
@@ -10,14 +8,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
     styleUrl: './user.component.css'
 })
 export class UserComponent {
-    selectedUser = DUMMY_USERS[randomIndex];
+    @Input() id!: string;
+    @Input({ required: true }) avatar!: string;
+    @Input({ required: true }) name!: string; 
+    @Output() select = new EventEmitter<string>(); 
+
 
     get imagePath() {
-        return 'assets/users/' + this.selectedUser.avatar
+        return 'assets/users/' + this.avatar;
     }
 
-    onSelectUser() {
-        console.log("Clicked!")
+    onSelectUserEmit() {
+        this.select.emit(this.id);
     }
 
 }
